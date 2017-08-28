d1cb19c4c72083d7c073e0349b1f4b2e0d3a01f2
johnabass <johnbass1017@yahoo.com>
2017-08-28 12:29:39 -0700

    interim commit

--- health2/stat.go
+++ health2/stat.go
@@ -1,5 +1,29 @@
 package health2
 
-type Stat interface {
-	Update(value float64)
+import "sync/atomic"
+
+type Resetter interface {
+	Reset()
+}
+
+type Value interface {
+	Resetter
+	Set(value int64)
+	Add(delta int64)
+}
+
+type basicStat struct {
+	value int64
+}
+
+func (b *basicStat) Reset() {
+	atomic.StoreInt64(&b.value, 0)
+}
+
+func (b *basicStat) Set(value int64) {
+	atomic.StoreInt64(&b.value, value)
+}
+
+func (b *basicStat) Add(delta int64) {
+	atomic.AddInt64(&b.value, delta)
 }
